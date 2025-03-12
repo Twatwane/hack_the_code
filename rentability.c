6c058b1b46a61ec92534edbd50dcf09eb2979361
root <root@LAPTOP-RAT4PNM8.localdomain>
2025-03-12 18:36:20 +0100

    rentability

--- rentability.c
+++ rentability.c
@@ -1,5 +1,36 @@
-// #include "main.h"
+#include "main.h"
 
-// int rentability(Resource r, int day) { // renvoie la rentabilite d'une ressource pour un nombre de jour day
+void active(Resource *r) {
+	Resource info = *r;
+	if ((*r).isObsolete) {
+		return ;
+	}
+	(*r).RL--;
+	if ((*r).RL == 0) {
+		(*r).isObsolete = 1;
+	}
+	if ((*r).RW > 0) {
+		(*r).RW--;
+	}
+	else {
+		if ((*r).RM > 0) {
+			(*r).RM--;
+		}
+		else {
+			(*r).RM = info.RM;
+			(*r).RW = info.RW;
+		}
+	}
+	(*r).isActive = (*r).RL > 0;
+}
 
-// }
+int rentability(Resource r, int day) { // renvoie la rentabilite d'une ressource pour un nombre de jour day
+	int cost = 0;
+	int activity = 0;
+	int durability = 0;
+
+	cost += r.RA;
+	while (!r.isObsolete) {
+		
+	}
+}
