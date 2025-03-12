b43ca0414418ccfb61597ab6f2e65c1343253cb5
Chaisneau <chaisneau@gmail.com>
2025-03-12 17:43:32 +0100

    get profit fixed

--- game_utility.c
+++ game_utility.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
-int get_profit(int powered_building, int turn_num)
-{
-	return min(powered_building, turns[turn_num].TX) * turns[turn_num].TR;
+int get_profit(int powered_buildings, int turn_index) {
+    if (powered_buildings < turns[turn_index].TM) {
+        return 0; // No profit if below TM
+    }
+    return min(powered_buildings, turns[turn_index].TX) * turns[turn_index].TR;
 }
