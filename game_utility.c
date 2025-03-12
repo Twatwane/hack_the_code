6d7a7aa68a075878454647fd6d17aa5ef54efed3
Chaisneau <chaisneau@gmail.com>
2025-03-12 19:05:14 +0100

    active on get powered

--- game_utility.c
+++ game_utility.c
@@ -12,7 +12,8 @@ int get_profit(int powered_buildings, int turn_index) {
 int get_powered_buildings() {
     int total_powered = 0;
     for (int i = 0; i < R; i++) {
-        total_powered += ressources_buyed[i].RU;
+        if (ressources_buyed[i].isActive)
+            total_powered += ressources_buyed[i].RU;
     }
     a(&total_powered); // Apply Smart Meter adjustments
     return total_powered;
