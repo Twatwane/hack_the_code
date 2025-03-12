d0b3ea7cff6c88af4693890c838f29510938cfac
Chaisneau <chaisneau@gmail.com>
2025-03-12 17:46:37 +0100

    get powered building

--- game_utility.c
+++ game_utility.c
@@ -6,3 +6,12 @@ int get_profit(int powered_buildings, int turn_index) {
     }
     return min(powered_buildings, turns[turn_index].TX) * turns[turn_index].TR;
 }
+
+int calculate_powered_buildings() {
+    int total_powered = 0;
+    for (int i = 0; i < R; i++) {
+        total_powered += ressources_buyed[i].RU;
+    }
+    a(&total_powered); // Apply Smart Meter adjustments
+    return total_powered;
+}
